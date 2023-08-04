4bd3f454d384738c4a8a4a3ed1d13d5e94665127
CruzAndrei <cruzandrei237@gmail.com>
2023-08-05 00:15:01 +0800

    pseudocode of the program

---
+++ Car Class.py
@@ -0,0 +1,14 @@
+# Constructor, takes arguments and constructs the class
+        
+#Accelerate method
+#" Accelerate car. "
+#brake method
+#" Decelerate car. "
+#get_speed method
+#" Get current speed of car. "
+
+#car method
+# create a Car object
+# calls the accelerate method
+
+# calls the brake method five times.
