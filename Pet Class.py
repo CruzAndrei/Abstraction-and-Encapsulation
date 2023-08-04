7b174cf347bcbe0ba906c33c70f7fd21780c982b
CruzAndrei <cruzandrei237@gmail.com>
2023-08-05 00:31:17 +0800

    pesudocode of the program

---
+++ Pet Class.py
@@ -0,0 +1,15 @@
+#constructor with arugments
+#name method to return name
+#name method to set the name
+#animal type to return type
+#animal type to set type
+#age type to return age
+#age type to set age
+
+# Creating the pet object
+#Asking user for inputs
+#name
+#type
+#age
+
+#print result
